76057baa05ba68280b8d0a8fbd0ed1c007ff421d
Julius Pabrinkis <julius@cannon.lt>
2015-12-15 23:18:50 +0100

    Make first accepting TCP server which works with OpenCPN

--- server.rb
+++ server.rb
@@ -1,10 +1,20 @@
 require 'socket'
+require 'pry'
+host = '127.0.0.1'
+port = 7000
+server = TCPServer.new(host, port)
+p "Initilize server #{host}:#{port}"
 
-server = TCPServer.new('localhost', 7000)
 loop do
   Thread.start(server.accept) do |client|
-    client.puts "Hello !"
-    client.puts "Time is #{Time.now}"
-    client.close
+    while line = client.gets do
+      p "#{Time.now} - #{line}"
+    end
   end
 end
+
+Example
+# "2015-12-15 23:17:21 +0100 - $ECRMB,A,0.000,L,001,002,3609.727,N,00522.316,W,0.821,291.359,nan,V*48\r\n"
+# "2015-12-15 23:17:21 +0100 - $ECRMC,221721,A,3609.429,N,00521.371,W,nan,nan,151215,1.353,W*65\r\n"
+# "2015-12-15 23:17:21 +0100 - $ECAPB,A,A,0.00,L,N,V,V,291.27,T,002,291.36,T,291.36,T*3E\r\n"
+# "2015-12-15 23:17:21 +0100 - $ECXTE,A,A,0.000,L,N*4F\r\n"
